b78a5cd71264611d9d4f4d1645a11a60713698d1
mashhadi <mashhadi.naghmeh@gmail.com>
2016-09-17 17:11:10 +0430

    spaces added to appkernel and composer.lock

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -1,6 +1,8 @@
 <?php
+
 use Symfony\Component\HttpKernel\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
+
 class AppKernel extends Kernel
 {
     public function registerBundles()
@@ -17,29 +19,33 @@ class AppKernel extends Kernel
 //            new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
-            new Vich\UploaderBundle\VichUploaderBundle(),
-            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
         ];
+
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
         }
+
         return $bundles;
     }
+
     public function getRootDir()
     {
         return __DIR__;
     }
+
     public function getCacheDir()
     {
         return dirname(__DIR__).'/var/cache/'.$this->getEnvironment();
     }
+
     public function getLogDir()
     {
         return dirname(__DIR__).'/var/logs';
     }
+
     public function registerContainerConfiguration(LoaderInterface $loader)
     {
         $loader->load($this->getRootDir().'/config/config_'.$this->getEnvironment().'.yml');
