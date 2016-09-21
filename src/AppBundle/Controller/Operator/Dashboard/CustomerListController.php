e0aef80df3fa8909e4fd7e5c9e704cf9c505a736
Sadegh Mesripour <sadegh.mesripour@gmail.com>
2016-09-21 10:39:43 +0330

    get customer list and show that

--- src/AppBundle/Controller/Operator/Dashboard/CustomerListController.php
+++ src/AppBundle/Controller/Operator/Dashboard/CustomerListController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller\Operator\Dashboard;
 
+use AppBundle\Entity\Customer;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
@@ -11,9 +12,16 @@ class CustomerListController extends Controller
      * @Route("/customerList",name="operator_dashboard_customerList")
      */
     public function getAllCustomerAction(){
+        $customerTbl = new Customer();
+        $customerList = $this->getDoctrine()
+            ->getRepository("AppBundle:Customer")
+            ->findAll();
         return $this->render
         (
-            'operator/dashboard/customerList/customerList.html.twig'
+            'operator/dashboard/customerList/customerList.html.twig',
+            [
+                'customerList'=>$customerList
+            ]
         );
     }
 }
