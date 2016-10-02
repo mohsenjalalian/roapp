5f37af41ef09457200d48052053ed62d1fb9c256
Saeed Asalisaf <asalisaf.saeed@gmail.com>
2016-10-02 21:25:07 +0330

    entities fixed!

--- src/AppBundle/Entity/Customer.php
+++ src/AppBundle/Entity/Customer.php
@@ -4,57 +4,26 @@ namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
-use Symfony\Component\Validator\Constraints as Assert;
-use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
-
 
 /**
  * Customer
  * @ORM\Entity(repositoryClass="AppBundle\Repository\CustomerRepository")
- * @UniqueEntity(fields="username", message="Username already taken")
  */
-class Customer extends Person implements \Serializable
+class Customer extends Person implements UserInterface, \Serializable
 {
+    /**
+     * @ORM\Column(name="email", type="string", length=255, unique=true, nullable=true)
+     *
+     */
+    private $email;
 
     /**
-     * @ORM\Column(type="string", length=255, unique=true)
-     * @Assert\NotBlank()
-     */
-    private $username;
-    /**
-     * @ORM\Column(name="password", type="string")
+     * @ORM\Column(name="password", type="string", nullable=true)
      */
     private $password;
+    
     /**
-     * @ORM\Column(name="is_active", type="boolean")
-     */
-    private $isActive;
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="api_key", type="string", length=255, unique=true)
-     */
-    private $apiKey;
-
-    public function __construct()
-    {
-        $this->isActive = true;
-    }
-
-    /**
-     * Returns the roles granted to the user.
-     *
-     * <code>
-     * public function getRoles()
-     * {
-     *     return array('ROLE_USER');
-     * }
-     * </code>
-     *
-     * Alternatively, the roles might be stored on a ``roles`` property,
-     * and populated in any number of different ways when the user object
-     * is created.
-     * @return array (Role|string)[] The user roles
+     * @inheritdoc
      */
     public function getRoles()
     {
@@ -62,81 +31,53 @@ class Customer extends Person implements \Serializable
     }
 
     /**
-     * Returns the password used to authenticate the user.
-     *
-     * This should be the encoded password. On authentication, a plain-text
-     * password will be salted, encoded, and then compared to this value.
-     *
-     * @return string The password
+     * @inheritdoc
      */
     public function getPassword()
     {
         return $this->password;
-        // TODO: Implement getPassword() method.
     }
 
 
     /**
-     * Returns the salt that was originally used to encode the password.
-     *
-     * This can return null if the password was not encoded using a salt.
-     *
-     * @return string|null The salt
+     * @inheritdoc
      */
     public function getSalt()
     {
         return null;
-        // TODO: Implement getSalt() method.
     }
 
     /**
-     * Returns the username used to authenticate the user.
-     *
-     * @return string The username
+     * @inheritdoc
      */
     public function getUsername()
     {
-        return $this->username;
-        // TODO: Implement getUsername() method.
+        return $this->email;
     }
 
-
     /**
-     * Removes sensitive data from the user.
-     *
-     * This is important if, at any given point, sensitive information like
-     * the plain-text password is stored on this object.
+     * @inheritdoc
      */
     public function eraseCredentials()
     {
-        // TODO: Implement eraseCredentials() method.
     }
 
     /**
-     * String representation of object
-     * @link http://php.net/manual/en/serializable.serialize.php
-     * @return string the string representation of the object or null
-     * @since 5.1.0
+     * @inheritdoc
      */
     public function serialize()
     {
         return serialize(array(
             $this->id,
-            $this->username,
+            $this->email,
             $this->password,
             // see section on salt below
             // $this->salt,
         ));
-        // TODO: Implement serialize() method.
     }
+
     /**
-     * Constructs the object
-     * @link http://php.net/manual/en/serializable.unserialize.php
-     * @param string $serialized <p>
-     * The string representation of the object.
-     * </p>
-     * @return void
-     * @since 5.1.0
+     * @inheritdoc
      */
     public function unserialize($serialized)
     {
@@ -147,42 +88,22 @@ class Customer extends Person implements \Serializable
             // see section on salt below
             // $this->salt
             ) = unserialize($serialized);
-        // TODO: Implement unserialize() method.
     }
 
     /**
-     * Set apiKey
+     * Set email
      *
-     * @param string $apiKey
-     *
-     * @return Customer
-     */
-    public function setApiKey($apiKey)
-    {
-        $this->apiKey = $apiKey;
-        return $this;
-    }
-    /**
-     * Get apiKey
-     *
-     * @return string
-     */
-    public function getApiKey()
-    {
-        return $this->apiKey;
-    }
-    /**
-     * Set username
-     *
-     * @param string $username
+     * @param string $email
      *
      * @return Customer
      */
-    public function setUsername($username)
+    public function setUsername($email)
     {
-        $this->username = $username;
+        $this->email = $email;
+
         return $this;
     }
+
     /**
      * Set password
      *
@@ -193,27 +114,7 @@ class Customer extends Person implements \Serializable
     public function setPassword($password)
     {
         $this->password = $password;
+
         return $this;
-    }
-    /**
-     * Set isActive
-     *
-     * @param boolean $isActive
-     *
-     * @return Customer
-     */
-    public function setIsActive($isActive)
-    {
-        $this->isActive = $isActive;
-        return $this;
-    }
-    /**
-     * Get isActive
-     *
-     * @return boolean
-     */
-    public function getIsActive()
-    {
-        return $this->isActive;
     }
 }
--- src/AppBundle/Entity/Person.php
+++ src/AppBundle/Entity/Person.php
@@ -4,6 +4,8 @@ namespace AppBundle\Entity;
 
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity
@@ -11,8 +13,9 @@ use Doctrine\ORM\Mapping as ORM;
  * @ORM\InheritanceType("SINGLE_TABLE")
  * @ORM\DiscriminatorColumn(name="type", type="string")
  * @ORM\DiscriminatorMap({"customer" = "Customer", "driver" = "Driver"})
+ * @UniqueEntity(fields="username", message="Username already taken")
  */
-abstract class Person implements UserInterface
+abstract class Person
 {
     /**
      * @var int
@@ -30,6 +33,10 @@ abstract class Person implements UserInterface
      */
     protected $phone;
 
+    /**
+     * @ORM\Column(name="is_active", type="boolean")
+     */
+    protected $isActive = false;
 
     /**
      * Get id
@@ -63,5 +70,29 @@ abstract class Person implements UserInterface
     public function getPhone()
     {
         return $this->phone;
+    }
+
+    /**
+     * Set isActive
+     *
+     * @param boolean $isActive
+     *
+     * @return Customer
+     */
+    public function setIsActive($isActive)
+    {
+        $this->isActive = $isActive;
+        
+        return $this;
+    }
+    
+    /**
+     * Get isActive
+     *
+     * @return boolean
+     */
+    public function getIsActive()
+    {
+        return $this->isActive;
     }
 }
