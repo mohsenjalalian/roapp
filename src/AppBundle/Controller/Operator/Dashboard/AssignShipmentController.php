be12b71d4e82d48fb09c3aa06f54568f08fa2825
Sadegh Mesripour <sadegh.mesripour@gmail.com>
2016-10-05 10:27:14 +0330

    change assignShipment controller and views about that

--- src/AppBundle/Controller/Operator/Dashboard/AssignShipmentController.php
+++ src/AppBundle/Controller/Operator/Dashboard/AssignShipmentController.php
@@ -8,95 +8,29 @@ use Symfony\Component\Routing\Annotation\Route;
 use AppBundle\Entity\Shipment;
 use AppBundle\Entity\Driver;
 
+/**
+ * Class AssignShipmentController
+ * @package AppBundle\Controller\Operator\Dashboard
+ * @Route("/assign_shipment")
+ */
 class AssignShipmentController extends Controller
 {
+    
     /**
-     * @Route("/assign_shipment",name="app_operator_dashboard_assign_shipment_index")
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\Response
-     */
-    public function indexAction(Request $request)
-    {
-        $query = $this->getDoctrine()
-            ->getRepository('AppBundle:Shipment')
-            ->createQueryBuilder('s')
-            ->orderBy('s.pickUpTime', 'Asc')
-            ->getQuery()
-        ;
-
-        $paginator  = $this->get('knp_paginator');
-        $pagination = $paginator->paginate(
-            $query, /* query NOT result */
-            $request->query->getInt('page', 1)/*page number*/,
-            10/*limit per page*/
-        );
-
-        return $this->render(
-            ":operator/dashboard/assignShipment:index.html.twig",
-            [
-                'pagination' => $pagination
-            ]
-        );
-    }
-
-    /**
-     * @Route("/driver_assign/{shipment}",name="app_operator_dashboard_assign_shipment_driver_assign")
-     * @param Shipment $shipment
-     * @param Request $request
-     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
-     */
-    public function driverAssignAction(Shipment $shipment = null, Request $request)
-    {
-        // check shipment is exist
-        if ($shipment) {
-            // check shipment rejected by some driver or no
-            $banDriver = $this->get("app.shipment_assignment")
-                ->filterDriverAction($shipment);
-
-            $query = $this->getDoctrine()
-                ->getRepository('AppBundle:Driver')
-                ->createQueryBuilder('d')
-                ->orderBy('d.fullName', 'Asc')
-                ->getQuery()
-            ;
-            
-            $paginator  = $this->get('knp_paginator');
-            $pagination = $paginator->paginate(
-                $query, /* query NOT result */
-                $request->query->getInt('page', 1)/*page number*/,
-                10/*limit per page*/
-            );
-
-            return $this->render(
-                "operator/dashboard/assignShipment/driverListAssign.html.twig",
-                [
-                    'shipmentId'=>$shipment->getId(),
-                    'banDriverList'=>$banDriver,
-                    'pagination'=>$pagination
-                ]
-            );
-        } else {
-            return $this->redirectToRoute(
-                "app_operator_dashboard_assign_shipment_index"
-            );
-        }
-    }
-
-    /**
-     * @Route("/assign_set/{shipment}/{driver}",name="app_operator_dashboard_assign_shipment_assign_set")
+     * @Route("/match/{shipment}/{driver}",name="app_operator_dashboard_assign_shipment_match")
      * @param Shipment $shipment
      * @param Driver $driver
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function assignSetAction(Shipment $shipment = null, Driver $driver = null)
+    public function matchAction(Shipment $shipment = null, Driver $driver = null)
     {
         // if there is shipment and driver with given id
         if ($shipment && $driver) {
-            $sendedRequest = $this->get("app.shipment_assignment")
+            $sentRequest = $this->get("app.shipment_assignment")
                 ->sendRequest($shipment, $driver);
-            if ($sendedRequest) {
+            if ($sentRequest) {
                 return $this->redirectToRoute(
-                    "app_operator_dashboard_assign_shipment_index"
+                    "app_operator_dashboard_shipment_list"
                 );
             } else {
                 return $this->render(
@@ -108,7 +42,7 @@ class AssignShipmentController extends Controller
             }
         } else {
             return $this->redirectToRoute(
-                "app_operator_dashboard_assign_shipment_index"
+                "app_operator_dashboard_shipment_list"
             );
         }
     }
