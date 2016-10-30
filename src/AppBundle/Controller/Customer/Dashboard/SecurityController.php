5f0d187279b6ed94d084f99f8559438a171bb0e1
modiamir <amir.modarresi.13@gmail.com>
2016-10-30 21:22:14 +0330

    created customer registration createt customer activation

--- src/AppBundle/Controller/Customer/Dashboard/SecurityController.php
+++ src/AppBundle/Controller/Customer/Dashboard/SecurityController.php
@@ -4,17 +4,18 @@ namespace AppBundle\Controller\Customer\Dashboard;
 
 use AppBundle\Entity\Customer;
 
+use AppBundle\Form\Customer\Dashboard\RegisterType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class SecurityController extends Controller
 {
     /**
      * @author Naghmeh Mashhadi
-     * @Route("/login", name="login")
+     * @Route("/login", name="app_customer_dashboard_security_login")
      * This action displays and validates the login form
      */
     public function loginAction(Request $request)
@@ -46,8 +47,62 @@ class SecurityController extends Controller
      */
     public function logoutAction(Request $request)
     {
-        return new Response("You are logged out successfully");
+        return new Response("You are logged out successfully.");
     }
 
+    /**
+     * @Route("/register", name="_register")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     */
+    public function registerAction(Request $request)
+    {
+        $customer = new Customer();
+        $form = $this->createForm(RegisterType::class, $customer);
+        
+        $form->handleRequest($request);
+        
+        if ($form->isValid()) {
+            $em = $this->getDoctrine()
+                ->getManager();
 
+            $activationToken = md5(uniqid("", true));
+            $customer->setActivationToken($activationToken);
+
+            $em->persist($customer);
+            $em->flush();
+
+            $this->get('logger')->info('customer activation token', [$activationToken]);
+            
+            return $this->render(':customer/dashboard/security:registered.html.twig');
+        }
+
+        return $this->render('customer/dashboard/security/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route(path="/activation/{activationToken}")
+     */
+    public function activationAction($activationToken) {
+        $customer = $this->getDoctrine()
+            ->getManager()
+            ->getRepository('AppBundle:Customer')
+            ->findOneBy([
+                'activationToken' => $activationToken,
+                'isActive' => true
+            ]);
+
+        if (!$customer instanceof Customer) {
+            throw new NotFoundHttpException();
+        }
+
+        $customer->setIsActive(true);
+        
+        $this->getDoctrine()
+            ->getManager()
+            ->flush();
+
+        return $this->redirectToRoute('app_customer_dashboard_security_login');
+    }
 }
--- src/AppBundle/Entity/Customer.php
+++ src/AppBundle/Entity/Customer.php
@@ -3,13 +3,13 @@
 namespace AppBundle\Entity;
 
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\AdvancedUserInterface;
 
 /**
  * Customer
  * @ORM\Entity(repositoryClass="AppBundle\Repository\CustomerRepository")
  */
-class Customer extends Person implements UserInterface, \Serializable
+class Customer extends Person implements AdvancedUserInterface, \Serializable
 {
     /**
      * @ORM\Column(name="email", type="string", length=255, unique=true, nullable=true)
@@ -21,14 +21,6 @@ class Customer extends Person implements UserInterface, \Serializable
      * @ORM\Column(name="status", type="integer")
      */
     private $status;
-    /**
-     * Customer constructor
-     */
-
-    public function __construct()
-    {
-        $this->isActive = true;
-    }
 
     /**
      * @inheritdoc
@@ -75,6 +67,7 @@ class Customer extends Person implements UserInterface, \Serializable
             $this->password,
             // see section on salt below
             // $this->salt,
+            $this->isActive,
         ));
         // TODO: Implement serialize() method.
     }
@@ -90,6 +83,7 @@ class Customer extends Person implements UserInterface, \Serializable
             $this->password,
             // see section on salt below
             // $this->salt
+            $this->isActive,
             ) = unserialize($serialized);
         // TODO: Implement unserialize() method.
     }
@@ -154,5 +148,61 @@ class Customer extends Person implements UserInterface, \Serializable
     public function getEmail()
     {
         return $this->email;
+    }
+
+    /**
+     * Checks whether the user's account has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw an AccountExpiredException and prevent login.
+     *
+     * @return bool true if the user's account is non expired, false otherwise
+     *
+     * @see AccountExpiredException
+     */
+    public function isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is locked.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a LockedException and prevent login.
+     *
+     * @return bool true if the user is not locked, false otherwise
+     *
+     * @see LockedException
+     */
+    public function isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * Checks whether the user's credentials (password) has expired.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a CredentialsExpiredException and prevent login.
+     *
+     * @return bool true if the user's credentials are non expired, false otherwise
+     *
+     * @see CredentialsExpiredException
+     */
+    public function isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * Checks whether the user is enabled.
+     *
+     * Internally, if this method returns false, the authentication system
+     * will throw a DisabledException and prevent login.
+     *
+     * @return bool true if the user is enabled, false otherwise
+     *
+     * @see DisabledException
+     */
+    public function isEnabled() {
+        return $this->isActive;
     }
 }
