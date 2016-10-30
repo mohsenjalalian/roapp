b75ede851d6f59c535f7808e3b70929ec5583e15
Sadegh Mesripour <sadegh.mesripour@gmail.com>
2016-10-30 17:22:07 +0330

    register shipment complete

--- src/AppBundle/Form/Customer/Dashboard/ShipmentType.php
+++ src/AppBundle/Form/Customer/Dashboard/ShipmentType.php
@@ -5,6 +5,8 @@ namespace AppBundle\Form\Customer\Dashboard;
 use AppBundle\Form\DataTransformer\DateTimeTransformer;
 use Doctrine\ORM\EntityRepository;
 use jDateTime;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Console\Input\Input;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\CallbackTransformer;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -27,63 +29,32 @@ class ShipmentType extends AbstractType
         $builder
             ->add('description', null,
                 array(
-                    "label"=>"form.description",
+                    "label"=>"توضیحات",
                     'translation_domain' => 'messages',
                 ))
             ->add('value', null,
                 array(
-                    "label"=>"form.value",
+                    "label"=>"ارزش بسته",
                     'translation_domain' => 'messages'
                 ))
-//            ->add('status')
+
             ->add('pickUpTime', TextType::class,
                 array(
-                    "label"=>"form.pickUpTime",
+                    "label"=>"تعیین زمان تحویل",
                     'translation_domain' => 'messages',
                     'attr' => ['class' => 'js-datepicker']
-                ));
-//            ->add('createdAt', DateTimeType::class)
-//            ->add('price')
-//            ->add('type')
-//            ->add('ownerAddress',null,
-//                array(
-//                    "label"=>"form.ownerAddress",
-//                    'translation_domain' => 'messages',
-//                ))
-//            ->add('otherAddress', null,
-//                array(
-//                    "label"=>"other.address",
-//                    'translation_domain' => 'messages',
-//                    'mapped' => false
-//                ))
-//        ;
+                ))
+
+            ->add('other',TextType::class,
+                array(
+                    "label"=>"شماره گیرنده",
+                    'attr' => ['placeHolder'=> 'لطفا شماره تلفن همراه مورد نظر خود را وارد نمایید.'],
+                    'translation_domain' => 'messages',
+                    'mapped' => false
+                ))
+        ;
         $builder->get('pickUpTime')
             ->addModelTransformer(new DateTimeTransformer());
-//            ->addModelTransformer(new CallbackTransformer(
-//            function ($gDate) {
-//                if (empty($gDate)) {
-//                    return $gDate;
-//                }
-//                return $gDate->format("Y-m-d H:i:s");
-//            },
-//            function ($jdate) {
-//                $date = explode("-",substr($jdate,0,10));
-//                $time = substr($jdate,11,-3);
-//                $gregorianArr = jDateTime::toGregorian($date[0],$date[1],$date[2]);
-//                $gregorianDate = new \DateTime(
-//                    sprintf(
-//                        "%s-%s-%s %s",
-//                        $gregorianArr[0],
-//                        $gregorianArr[1],
-//                        $gregorianArr[2],
-//                        $time
-//                    )
-//                );
-//                return $gregorianDate;
-////                // transform the string back to an array
-////                return explode(',', $tagsAsString);
-//            }
-//        ));
     }
 
     /**
