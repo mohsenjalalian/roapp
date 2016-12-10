0aa64c192493ea4b2a91170f309cfb7d5943a026
mohsenjalalian <jalalianmohsen@gmail.com>
2016-12-10 18:02:48 +0330

    AccessControlInterface

---
+++ src/AppBundle/Security/AccessControlInterface.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * User: mohsenjalalian
+ * Date: 12/10/16
+ * Time: 5:58 PM
+ */
+
+namespace AppBundle\Security;
+
+/**
+ * Interface AccessControlInterface
+ * @package AppBundle\Security
+ */
+interface AccessControlInterface
+{
+}
