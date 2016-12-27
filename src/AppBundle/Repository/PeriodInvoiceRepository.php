e137cb7b0148a12b1156e940705db05be9779277
Sadegh Mesripour <sadegh.mesripour@gmail.com>
2016-12-27 12:56:40 +0330

    create period invoice after register shipment

--- src/AppBundle/Repository/PeriodInvoiceRepository.php
+++ src/AppBundle/Repository/PeriodInvoiceRepository.php
@@ -8,7 +8,9 @@
 
 namespace AppBundle\Repository;
 
+use AppBundle\Entity\AbstractInvoice;
 use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\NoResultException;
 
 /**
  * Class PeriodInvoiceRepository
@@ -16,4 +18,29 @@ use Doctrine\ORM\EntityRepository;
  */
 class PeriodInvoiceRepository extends EntityRepository
 {
+    /**
+     * @return array
+     */
+    public function getTodayPeriodInvoice()
+    {
+        $from = new \DateTime();
+        $until = new \DateTime();
+        $from = $from->setTime(00, 00, 00);
+        $until = $until->setTime(23, 59, 59);
+        try {
+            $periodInvoice = $this->createQueryBuilder('p')
+                ->Where('p.createdAt >= :from')
+                ->andwhere('p.createdAt <= :until')
+                ->andWhere('p.status=:status')
+                ->setParameter('from', $from)
+                ->setParameter('until', $until)
+                ->setParameter('status', AbstractInvoice::STATUS_UNPAID)
+                ->getQuery()
+                ->getSingleResult();
+        } catch (NoResultException $e) {
+            $periodInvoice = null;
+        }
+
+        return $periodInvoice;
+    }
 }
