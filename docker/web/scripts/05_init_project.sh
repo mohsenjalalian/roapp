d9721a1a7d8978fb7bb2c5af8ea996d3856f1bda
modiamir <amir.modarresi.13@gmail.com>
2017-01-03 18:12:22 +0330

    installed compass in docker web container

--- docker/web/scripts/05_init_project.sh
+++ docker/web/scripts/05_init_project.sh
@@ -4,5 +4,6 @@ setuser www-data bin/console cache:clear --env=dev
 setuser www-data bin/console doctrine:database:create
 setuser www-data bin/console doctrine:schema:update --force
 cd /var/www/src/AppBundle/Resources/public && setuser www-data bower install
+cd /var/www/src/AppBundle/Resources/public && setuser www-data compass compile
 cd /var/www/ && setuser www-data bin/console assets:install --symlink
 cd /var/www/ && setuser www-data bin/console assetic:dump
