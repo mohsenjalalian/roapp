5ae5fb541ad14ace6c461b5f0c6a77d5f1c1b47a
Sadegh Mesripour <sadegh.mesripour@gmail.com>
2017-01-18 16:20:39 +0330

    added distance & duration fields to show shipment page in customer dashboard

--- src/AppBundle/Resources/public/javascript/customer/dashboard/shipment/show/map.js
+++ src/AppBundle/Resources/public/javascript/customer/dashboard/shipment/show/map.js
@@ -8,6 +8,7 @@ var line;
 var show_map =$("#show-shipment-map");
 var other_address_lat = show_map.data('lat');
 var other_address_lng = show_map.data('lng');
+var currentStatus;
 
 // function initMap() {
 //     var tehran = {lat: 35.78819000, lng: 51.45983810};
@@ -27,9 +28,14 @@ $(document).ready(function () {
         var obj = JSON.parse(msg);
         newLat = obj.new_val.lat;
         newLng = obj.new_val.lng;
-
         oldLat = marker.position.lat();
         oldLng = marker.position.lng();
+
+        var origin1 = {lat: parseFloat(newLat), lng: parseFloat(newLng)};
+        var destinationB = {lat: parseFloat(other_address_lat), lng: parseFloat(other_address_lng)};
+        currentStatus = distanceMatrix(origin1,destinationB);
+        $(".google_distance td").html(currentStatus.Distance);
+        $(".google_time td").html(currentStatus.Duration);
 
         marker.setPosition( new google.maps.LatLng( newLat, newLng) );
         map.panTo( new google.maps.LatLng( newLat, newLng) );
@@ -47,7 +53,9 @@ $(document).ready(function () {
 
             var origin1 = {lat: parseFloat(lastLat), lng: parseFloat(lastLng)};
             var destinationB = {lat: parseFloat(other_address_lat), lng: parseFloat(other_address_lng)};
-            distanceMatrix(origin1, destinationB);
+            currentStatus = distanceMatrix(origin1, destinationB);
+            $(".google_distance td").html(currentStatus.Distance);
+            $(".google_time td").html(currentStatus.Duration);
 
             marker = new google.maps.Marker({
                 position: new google.maps.LatLng(lastLat, lastLng),
